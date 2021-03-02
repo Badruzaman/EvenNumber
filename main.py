fcf28ee90b502a86ede8e692a929cb8e54be30f0
Badruzzaman <badru.cse@gmail.com>
2021-03-02 16:55:42 +0600

    first commit from pycharm

--- main.py
+++ main.py
@@ -3,5 +3,3 @@ list = [1,2,3,4,5,6,7]
 for item in range(len(list)):
     if list[item] % 2 == 0:
         print(list[item] , ' is even number')
-    else:
-        print(list[item] , ' is odd number')
