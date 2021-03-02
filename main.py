de751e019c4aae588d62fc60e2c8a253665c70cc
Badruzzaman <badru.cse@gmail.com>
2021-03-02 15:56:45 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ list = [1,2,3,4,5,6,7]
 for index in range(len(list)):
     if list[index] % 2 == 0:
         print(list[index] , ' is even number')
-    else:
-        print(list[index] , ' is odd number')
+    
